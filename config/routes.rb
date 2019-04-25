a67e4ade4f4a93c08a8677f288fcf66151f69c53
kazusa.saito <3110kazusa@gmail.com>
2019-04-26 02:16:10 +0900

    feat: profile機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # root to: 'home#index'
   root to: 'posts#index'
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users,
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks',
+                 registrations: 'registrations' }
+
+  get '/users/:id', to: 'users#show', as: 'user'
 
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
