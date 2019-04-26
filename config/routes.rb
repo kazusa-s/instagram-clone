d281043aed800552bd96af8ff851b7fc9cd105b7
saito <>
2019-04-26 18:10:29 +0900

    feat: コメント機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
 
   resources :posts, only: %i(new create index show destroy) do
     resources :photos, only: %i(create)
+    resources :comments, only: %i(create destroy)
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_23_130350) do
+ActiveRecord::Schema.define(version: 2019_04_26_073842) do
 
-  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "comment", null: false
+    t.bigint "post_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
     t.string "image", null: false
     t.bigint "post_id", null: false
     t.datetime "created_at", null: false
@@ -20,7 +33,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_130350) do
     t.index ["post_id"], name: "index_photos_on_post_id"
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "posts", force: :cascade do |t|
     t.string "caption"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
@@ -28,7 +41,7 @@ ActiveRecord::Schema.define(version: 2019_04_23_130350) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -44,6 +57,8 @@ ActiveRecord::Schema.define(version: 2019_04_23_130350) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "comments", "posts"
+  add_foreign_key "comments", "users"
   add_foreign_key "photos", "posts"
   add_foreign_key "posts", "users"
 end
